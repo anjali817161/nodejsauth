67e7f84a931e083e868777f497dd1880680f6e3a
anjali817161 <137703494+anjali817161@users.noreply.github.com>
2023-11-29 20:49:40 +0530

    authentication
    authentication

---
+++ config/key.js
@@ -0,0 +1,19 @@
+// module.exports = {
+//     MongoURI: "mongodb://admin:admin987@cluster0-shard-00-00.r3fs6.mongodb.net:27017,cluster0-shard-00-01.r3fs6.mongodb.net:27017,cluster0-shard-00-02.r3fs6.mongodb.net:27017/auth?authSource=admin&replicaSet=atlas-638q0p-shard-0&readPreference=primary&ssl=true"
+// }
+
+// // mongodb://127.0.0.1:27017/codeial
+
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://chaudharyanjali6300:XtKLjfPEsu3DlR27@cluster0.1a8qdp5.mongodb.net/?retryWrites=true&w=majority');
+
+const db = mongoose.connection;
+
+db.on('error',console.error.bind(console,"Error connecting to MongoDB"));
+
+db.once('open',function(){
+    console.log("Connected to Database :: MongoDB");
+});
+
+module.exports = db;
